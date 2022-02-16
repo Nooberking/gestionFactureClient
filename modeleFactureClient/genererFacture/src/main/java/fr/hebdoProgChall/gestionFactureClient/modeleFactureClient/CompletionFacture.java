73d8a6ed81c8ffaa423361ded6ebd2750d3d8dca
nooberking-Desktop <davidmbmanuel@gmail.com>
2022-02-16 01:09:37 +0100

    copie réussie

--- modeleFactureClient/genererFacture/src/main/java/fr/hebdoProgChall/gestionFactureClient/modeleFactureClient/CompletionFacture.java
+++ modeleFactureClient/genererFacture/src/main/java/fr/hebdoProgChall/gestionFactureClient/modeleFactureClient/CompletionFacture.java
@@ -6,15 +6,16 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class CompletionFacture {
 
-    public void generate(int id){
-        File src = new File("C:\\Users\\david\\IdeaProjects\\gestionFactureClient\\generationFacture\\src\\main\\resources\\Facture Template.xlsx");
-        File dest = new File("C:\\Users\\david\\IdeaProjects\\gestionFactureClient\\generationFacture\\src\\main\\resources\\factures\\Facture n°" + id + ".xlsx");
-
+    public void generate(int id) throws IOException {
+        File src = new File("C:\\Users\\david\\IdeaProjects\\gestionFactureClient\\modeleFactureClient\\genererFacture\\src\\main\\resources\\Facture Template.xlsx");
+        File dest = new File("C:\\Users\\david\\IdeaProjects\\gestionFactureClient\\modeleFactureClient\\genererFacture\\src\\main\\resources\\factures\\Facture n°" + id + ".xlsx");
+        Files.copy(src.toPath(),dest.toPath());
         XSSFWorkbook wk  = new XSSFWorkbook();
     }
 
